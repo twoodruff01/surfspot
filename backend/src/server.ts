28ab51607183a298f57f5ab4f2e462f8a0c578f7
Tom Woodruff <tomwoodruff97@gmail.com>
2023-06-07 16:18:27 +1000

    Finished setting up hard-coded database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,30 @@
 import express, { Express, Request, Response } from 'express';
 import { config } from 'dotenv'
+// import { v4 as uuid } from 'uuid'
+
+import { fakeDb } from './fakeDb';
 
 config()
 
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is running.');
+function setupRes(res: Response) {
+    // res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Content-Type', 'application/json')
+    return res
+}
+
+app.get('/topbreaks', (req: Request, res: Response) => {
+    setupRes(res).end(JSON.stringify(fakeDb.breaks))
 });
 
-app.get('/getSomething', (req: Request, res: Response) => {
-    console.log('get request')
-    res.set('Access-Control-Allow-Origin', '*')
-    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ key: 'something' }));
+app.get('/forecast/:id', (req: Request, res: Response) => {
+    let { id } = req.params
+    setupRes(res).send(JSON.stringify(fakeDb.forecasts[id]))
 })
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`)
 });
---
+++ backend/src/types.ts
@@ -0,0 +1,37 @@
+export interface SurfbreakDetailedForecast {
+    basicInfo: SurfbreakBasicInfo,
+    location: {
+        region: string,
+        coordinates: Point
+    },
+    forecast: Forecast
+}
+
+export interface SurfbreakBasicInfo {
+    name: string
+    id: string,  // No type support... should be a uuid
+}
+
+export interface Point {
+    latitude: number,
+    longitude: number
+}
+
+export interface Forecast {
+    labels: string[],
+    periods: number[],
+    heights: number[],
+    directions: number[]
+    windSpeed: number[],
+    windDirection: number[],
+    tide: number[]
+}
+
+export type Forecasts = {
+    [key: string]: SurfbreakDetailedForecast
+}
+
+export interface FakeDb {
+    forecasts: Forecasts,
+    breaks: SurfbreakBasicInfo[]
+}
